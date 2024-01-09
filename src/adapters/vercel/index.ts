cbbf6c9ec491b207a7553f03c253b084c1f6be5b
Joseph Lozano <me@lozanojoseph.com>
2024-01-08 22:01:32 -0500

    vc-config

--- src/adapters/vercel/index.ts
+++ src/adapters/vercel/index.ts
@@ -2,18 +2,19 @@ import { dirname, join } from "path";
 import { Adapter } from "../index";
 import { node } from "unenv";
 import { fileURLToPath } from "url";
-import {cpSync, readdirSync} from "fs";
+import {cpSync, mkdirSync, readdirSync} from "fs";
 
 export default () =>
   Adapter({
     name: "vercel",
     runtime: join(dirname(fileURLToPath(import.meta.url)), "entry"),
     outDir: ".vercel/output/static",
-    serverDir: ".vercel/output/functions",
+    serverDir: ".vercel/output/functions/.func",
     publicDir: ".vercel/output/static",
     entryName: "index",
     env: node,
     onBuild() {
       cpSync("node_modules/@vonojs/vono/dist/adapters/vercel/config.json", ".vercel/output/config.json")
+      cpSync("node_modules/@vonojs/vono/dist/adapters/vercel/vc-config.json", ".vercel/output/functions/.func/.vc-config.json")
     },
   });
